cd271204479a9d48a478a9a03502cc36edfdffb9
kodeebo <Torgeirh@Torgeirs-MacBook-Pro.local>
2016-06-21 15:17:18 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+//TODO:
+//no jquery
+//handlebars for the insertion of table rows
+$(document).ready(function() {
+
+  var page = 1;
+  var url = "https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=20";
+  var source = $("#table-template").html();
+  var template = Handlebars.compile(source);
+
+  var makeCall = function(url) {
+    $.getJSON(url, function(data) {
+    var html = template(data);
+    $('.table').empty().append(html);
+
+    }); //End of getJSON
+  };
+
+  
+  makeCall(url);
+
+  document.getElementById("next").onclick=function() {
+    if(page < 49) {
+    page += 1;
+    makeCall(url + "&page=" + page);
+ $('body').animate({scrollTop:$('table').offset().top},500);
+    updateCount();
+    }
+  }
+    document.getElementById("prev").onclick=function() {
+    if(page > 1) {
+      page -= 1; 
+      makeCall(url + "&page=" + page);
+ $('body').animate({scrollTop:$('table').offset().top},500);
+      updateCount();
+    }
+  }
+  
+  var updateCount = function() {
+    $("#pageCount").html("Page " + page + " of 49");
+  }
+  
+});
